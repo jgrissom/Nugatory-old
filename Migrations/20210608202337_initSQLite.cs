62b4e1260543dc75d22c15f164353e2b3cebdf13
jgrissom <jgrissom91@gmail.com>
2021-06-08 15:24:02 -0500

    Add Migration - dotnet ef migrations add initSQLite

---
+++ Migrations/20210608202337_initSQLite.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WordApi.Migrations
+{
+    public partial class initSQLite : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "WordColors",
+                columns: table => new
+                {
+                    WordColorId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Word = table.Column<string>(type: "TEXT", nullable: true),
+                    Color = table.Column<string>(type: "TEXT", nullable: true),
+                    TimeStamp = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WordColors", x => x.WordColorId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WordColors");
+        }
+    }
+}
