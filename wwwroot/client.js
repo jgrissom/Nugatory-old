46e05c8d8b4e99abba21d887af32b824c809d891
jgrissom <jgrissom91@gmail.com>
2021-06-12 22:12:51 -0500

    refactor javascript

--- wwwroot/client.js
+++ wwwroot/client.js
@@ -1,3 +1,5 @@
+"use strict";
+
 // get container width and height
 const container = document.getElementById('container');
 const width = container.clientWidth;
@@ -11,30 +13,41 @@ window.onload = function() {
 function getData(url) {
     fetch(url)
         .then(response => response.json())
-        .then(data => placeWords(data))
+        .then(data => {
+            placeWords(data);
+            showWords();
+        })
         .catch(error => {
             console.error('There has been a problem with your fetch operation:', error);
         });
 }
 
-// place words in window at random top, left
 function placeWords(words){
     console.log(words);
     if (words) {
-        words.forEach(word => {
-            const id = word.id;
-            container.innerHTML += '<div style="opacity:0;color:' + word.color + '" class="word in" id="' + id + '">' + word.word + '</div>';
-            const div = document.getElementById(id);
-            // update top and left position
-            div.style.top = getRandomNumber(0, height - div.clientHeight) +"px";
-            div.style.left = getRandomNumber(0, width - div.clientWidth) +"px";
-        });
-        const divs = document.getElementsByClassName('word');
-        for ( let i=0; i < divs.length; i++ ) {
-            const div = divs[i];
-            div.style.opacity = .75;
-        }
+        words.forEach(word => placeWord(word));
     }
+}
+
+// place word in window at random top, left
+function placeWord(word) {
+    const id = word.id;
+    container.innerHTML += '<div style="opacity:0;color:' + word.color + '" class="word in" id="' + id + '">' + word.word + '</div>';
+    const div = document.getElementById(id);
+    // update top and left position
+    div.style.top = getRandomNumber(0, height - div.clientHeight) +"px";
+    div.style.left = getRandomNumber(0, width - div.clientWidth) +"px";
+}
+
+function showWords() {
+    const divs = document.getElementsByClassName('word');
+    for ( let i = 0; i < divs.length; i++ ) {
+        showWord(divs[i]);
+    }
+}
+
+function showWord(wordDiv) {
+    wordDiv.style.opacity = .75;
 }
 
 function getRandomNumber(min, max) {
@@ -56,3 +69,28 @@ document.getElementById('container').onclick = function(event) {
             });
     }
 };
+
+//------------------------- signalR -----------------------------------------
+var connection = new signalR.HubConnectionBuilder().withUrl("/nugatoryHub").build();
+
+// initialize signalR connection
+connection.start().then(function () {
+    console.log('connection started');
+});
+
+// upon receipt of add message, add the word to the DOM
+connection.on("ReceiveAddMessage", function (obj) {
+    placeWord(obj);
+    showWord(document.getElementById(obj["id"]));
+});
+
+// upon receipt of delete message, remove the word from the DOM
+connection.on("ReceiveDeleteMessage", function (id) {
+    const obj = document.getElementById(id);
+    obj.classList.remove('in');
+        obj.classList.add('out');
+        obj.style.opacity = 0;
+        obj.addEventListener("transitionend", () => {
+            obj.remove();
+        });
+});
